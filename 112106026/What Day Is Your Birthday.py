f75167d57bf4a9b32716d518d1ede136858b3dc2
Yuka <106221406+Duckpaper@users.noreply.github.com>
2025-01-06 17:37:46 +0800

    Day2-3

---
+++ 112106026/What Day Is Your Birthday.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    html = '''
+    <form action="/whatday" method="get">
+        <input type=date name=birthday>
+        <input type=submit>
+    </form>
+    '''
+    return html
+
+@app.route('/whatday')
+def whatday():
+    birthday_str = request.values['birthday']
+
+    birthday = datetime.strptime(birthday_str, "%Y-%m-%d")
+    weekdays = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
+    weekday_name = weekdays[birthday.isoweekday() - 1]
+    return f"{birthday_str} is {weekday_name}"
