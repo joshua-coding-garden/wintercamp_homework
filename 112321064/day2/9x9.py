44a8350bd0bf812b5c72dcfd03b4b0910bc3c1f5
112321064 <shineshine6023@gmail.com>
2025-01-06 18:26:44 +0800

    Create 9x9.py

---
+++ 112321064/day2/9x9.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    html = '<table border>\n'
+    for i in range(1, 10):
+        html += '<tr>'
+        for j in range(1, 10):
+            if (i+j) % 2 == 0:
+                html += '<td style="background-color: pink;">'
+            else:
+                html += '<td>'
+            html += "{} * {} = {}</td>".format(i, j, i*j)
+        html += '</tr>\n'
+    html += '</table>\n'
+    return html
