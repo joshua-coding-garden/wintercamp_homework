a938f50399ca9eaab2d3fc17511097fe28bea0ea
zzc06 <157600724+zzc06@users.noreply.github.com>
2025-01-07 20:12:07 +0800

    Create favicon.py

---
+++ 111321065/favicon.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+app = Flask(__name__)
+@app.route('/')
+def index():
+    html = '''
+    <link rel="shortcut icon" href="/static/icon.ico">
+    <form action='/upload' method='POST'
+    enctype = "multipart/form-data">
+    Select a file to upload:
+    <input type="file" name="file"><br>
+    <input type="submit">
+    </form>'''
+    return html
+@app.route('/upload', methods=['POST'])
+def upload():
+    f = request.files['file']
+    if f.filename != '':
+        fn = f.filename
+        f.save('static/' + fn)
+        return 'File <a href="static/{}">{}</a> uploaded successfully'.format(fn, fn)
+    else:
+        return 'Please select a file.'
