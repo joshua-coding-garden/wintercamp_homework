0e5951334b1e4123287dd4e7af782c6eb1347abc
luke <luke656595@gmail.com>
2025-01-14 17:51:05 +0800

    shawn-tsai

--- 112321022/Day4/NTUEEFaculty.py
+++ 112321022/Day4/NTUEEFaculty.py
@@ -6,10 +6,8 @@ html = urlopen(url).read().decode()
 bsObj = BeautifulSoup(html, "html.parser")
 
 name = bsObj.findAll("td", {"class":"teacher_list_title"})
-email = bsObj.findAll("a", {"style":"position:absolute; cursor:default;"})
+email = bsObj.select("a > img[src *= 'program/email_image.php']")
 group = bsObj.findAll("td", {"class": "teacher_list_title2"}, string=lambda x: x and "組別：" in x)
-
 for i, (n, e, g) in enumerate(zip(name, email, group), start=1):
-    img = e.find("img")
     t = g.get_text().strip()
-    print(i, n.get_text().strip(), img.get("alt"), t.replace("組別：", ""))
+    print(i, n.get_text().strip(), e.get("alt"), t.replace("組別：", ""))
